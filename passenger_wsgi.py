9efa446c9f634419c10dbfcb8981d1100e76214e
lvaka <mpstudiosEJShin@gmail.com>
2019-09-06 10:10:58 -0700

    deployed

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -14,7 +14,6 @@ if sys.executable != INTERP:
 # Add virtualenv packages to the start of the path
 sys.path.insert(0, os.path.join(env_dir, 'bin'))
 sys.path.insert(0, os.path.join(env_dir, 'lib', 'python3.6', 'site-packages'))
-#sys.path.insert(0, os.path.join(env_dir, 'lib', 'python3.6', 'site-packages', 'flask'))
 
 # (so it will be checked last).
 sys.path.append(project_dir)
--- run.py
+++
@@ -1,4 +0,0 @@
-from folio import app
-
-if __name__ == '__main__':
-	app.run()
