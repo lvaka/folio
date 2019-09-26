8ac88d9295d640edbe33aed13060c59c1783ddb5
lvaka <mpstudiosEJShin@gmail.com>
2019-09-25 17:12:02 -0700

    smtp-lib

--- folio/controllers.py
+++ folio/controllers.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, Response, render_template
-#from flask_mail import Mail, Message
-from flask_sendmail.mail import Mail, Message
+# from flask_mail import Mail, Message
+# from flask_sendmail.mail import Mail, Message
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from folio.forms.EmailForm import EmailForm
 import folio
 
@@ -12,20 +15,30 @@ def contact_submit():
         Post End Point for Email Messages
     """
 
-    mail = Mail(folio.app)
+    # mail = Mail(folio.app)
     form = EmailForm(request.form)
     if request.method == 'POST' and form.validate():
         name = form.name.data
         email = form.email.data
         message = form.message.data
-        #bodytext = message + "\n\n \"" + name + "\" <" + email +">"
         body = "%s \n\n \"%s\"<%s>" % (message, name, email)
         subject = "You Received a Message"
-        msg = Message(subject= subject,
-                    sender='noreply@ericjshin',
-                    recipients=['eric@ericjshin.com'])
-        msg.body = body
-        mail.send(msg)
+        msg = MIMEMultipart()
+        # msg = Message(subject,
+        #             sender='noreply@ericjshin',
+        #             recipients=['eric@ericjshin.com'])
+        # msg.body = body
+        # mail.send(msg)
+        msg['From'] = email
+        msg['To'] = 'eric@ericjshin.com'
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+        server = smtplib.SMTP('localhost')
+        server.sendmail('noreply@ericjshin.com', 
+                        'eric@ericjshin.com',
+                        msg.as_string()
+                        )
+        server.quit()
 
         return Response(status=200)
 
