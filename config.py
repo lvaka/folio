c3717a8e55e00a973f0c16af5901294145544473
Eric <eric@ericjshin.com>
2020-05-31 23:18:30 -0700

    Updated gitignore and requirements

--- config.py
+++
@@ -1,5 +0,0 @@
-DEBUG = True
-SECRET_KEY = 'P7H66hHpmZmPbJ6mfuZ1rdb0Zgw'
-SQLALCHEMY_DATABASE_URI = 'mysql://lvaka:root@localhost/ejs_portfolio?unix_socket=/var/run/mysqld/mysqld.sock'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SENDMAIL_LOCATION = '/usr/sbin/sendmail'
